badd7e7c30b45ec58de30edca448f41318f848bf
himanshupalsingh6-dev <himanshupalsingh6@gmail.com>
2026-01-04 15:00:25 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,7 @@
-// Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAWTOu3JBhg3JuZg6snAxhnf_XFhLhLkbc",
   authDomain: "quickpress-web.firebaseapp.com",
@@ -13,9 +11,6 @@ const firebaseConfig = {
   appId: "1:909067547966:web:fda5e47e1e58ca7ae7a86f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services (GLOBAL use ke liye)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
