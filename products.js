c4685138c1f4afb488d01d8ea7e502d532bd484c
himanshupalsingh6-dev <himanshupalsingh6@gmail.com>
2026-01-06 18:29:31 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,11 @@
+window.PRODUCTS = [
+  {id:1, name:"Cotton Shirt", cat:"Shirt", price:5, icon:"👕"},
+  {id:2, name:"Formal Shirt", cat:"Shirt", price:6, icon:"👔"},
+  {id:3, name:"Jeans Pant", cat:"Pant", price:7, icon:"👖"},
+  {id:4, name:"Cotton Pant", cat:"Pant", price:6, icon:"👖"},
+  {id:5, name:"Silk Saree", cat:"Saree", price:12, icon:"🥻"},
+  {id:6, name:"Woollen Sweater", cat:"Woollen", price:15, icon:"🧥"},
+  {id:7, name:"Winter Coat", cat:"Coat", price:18, icon:"🧥"},
+  {id:8, name:"Blazer", cat:"Blazer", price:20, icon:"🕴️"},
+  {id:9, name:"School Uniform", cat:"School", price:8, icon:"🎒"}
+];
